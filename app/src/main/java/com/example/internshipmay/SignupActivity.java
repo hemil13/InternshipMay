c785753f2a4a9065d2f25520c65a32d05ea318c9
Hemil13 <157737404+Hemil13@users.noreply.github.com>
2025-05-20 12:06:39 +0530

    SQLite Insert in Signup

--- app/src/main/java/com/example/internshipmay/SignupActivity.java
+++ app/src/main/java/com/example/internshipmay/SignupActivity.java
@@ -1,6 +1,7 @@
 package com.example.internshipmay;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,11 +23,16 @@ public class SignupActivity extends AppCompatActivity {
 
     String pattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
+    SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        db = openOrCreateDatabase("InternshipMay.db", MODE_PRIVATE, null);
+        String tableQuery = "CREATE TABLE IF NOT EXISTS user(userid INTEGER PRIMARY KEY AUTOINCREMENT ,name VARCHAR(50), email VARCHAR(100), contact VARCHAR(15), password VARCHAR(20))";
+        db.execSQL(tableQuery);
 
         have_account = findViewById(R.id.signup_have_account);
         name = findViewById(R.id.signup_name);
@@ -79,6 +85,9 @@ public class SignupActivity extends AppCompatActivity {
                     cnfpassword.setError("Password And Confirm Password Doesn't matches ");
                 }
                 else{
+                    String insertQuery = "INSERT INTO user VALUES(NULL,'"+name.getText().toString()+"','"+email.getText().toString()+"','"+contact.getText().toString()+"','"+password.getText().toString()+"')";
+                    db.execSQL(insertQuery);
+
                     Toast.makeText(SignupActivity.this, "Signup Successfully", Toast.LENGTH_LONG).show();
 //                    Intent intent = new Intent(SignupActivity.this, DashboardActivity.class);
 //                    startActivity(intent);
